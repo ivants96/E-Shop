35e375ed9a418ba2062b128cb350e0b8a02fd49b
Ivo <ivannovak@outlook.com>
2019-02-12 20:42:07 +0100

    Partialy Implementing AccountController - Implementing Login actions

--- E-Shop/Controllers/AccountController.cs
+++ E-Shop/Controllers/AccountController.cs
@@ -2,15 +2,71 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using E_Shop.Data.Models;
+using E_Shop.Models.AccountViewModels;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace E_Shop.Controllers
 {
+    [Authorize]
+    [Route("[controller]/[action]")]
     public class AccountController : Controller
     {
-        public IActionResult Index()
+        private UserManager<ApplicationUser> _userManager;
+        private SignInManager<ApplicationUser> _signInManager;
+
+        public AccountController(UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager)
         {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+        #region AccountController
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<IActionResult> LoginAsync(string returnUrl = null)
+        {
+            await HttpContext.SignOutAsync(IdentityConstants.ExternalScheme);
+            ViewData["ReturnUrl"] = returnUrl;
             return View();
         }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login(LoginViewModel model, string returnUrl = null)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, lockoutOnFailure: false);
+                if (result.Succeeded)
+                {
+                    return RedirectTolocal(returnUrl);
+                }
+                else
+                {
+                    return View(model);
+                }
+            }
+            return View(model);
+        }
+
+        private IActionResult RedirectTolocal(string returnUrl)
+        {
+            if (Url.IsLocalUrl(returnUrl))
+            {
+                return Redirect(returnUrl);
+            }
+            else
+            {
+                return RedirectToAction(nameof(HomeController.Index), "Home");
+            }
+        }
+
+        #endregion
+
     }
 }
